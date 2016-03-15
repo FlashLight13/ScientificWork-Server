9ec84c1fb293a26f3cb1884dce1186375538520d
Potapov Anton <potapov.a.p13@gmail.com>
2016-03-16 00:37:45 +0400

    Added web pages

---
+++ AlgoStadyingServer/web/pages/js/controllers/remove.js
@@ -0,0 +1,45 @@
+
+var request = null;
+function createRemove() 
+{
+    try {
+        request = new XMLHttpRequest();
+    } catch (trymicrosoft) {
+        try {
+            request = new ActiveXObject("MsXML2.XMLHTTP");
+        } catch (othermicrosoft) {
+            try {
+                request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (failed) {
+                request = null;
+            }
+        }
+    }
+
+    if (request == null)
+        alert("Error creating request object!");
+}
+
+function removeUser(id)
+{
+    createRemove()
+    var url = "/remove?"
+    + "id=" + id;
+
+    console.log(url);
+
+    request.onreadystatechange = handleRemove;
+    request.open("POST", url, true);
+    request.send(null);
+}
+
+function handleRemove() {
+    if (request.readyState==4)
+    { 
+        if (request.status==200) {
+            getUsers();
+        } else {
+            alert("Failed to remove the user")
+        }
+    }
+}
